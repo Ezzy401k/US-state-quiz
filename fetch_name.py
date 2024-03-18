ddac33a07547fd406bf647e637bfde96a5c4779e
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-18 14:33:18 +0300

    Update fetch_name.py

--- fetch_name.py
+++ fetch_name.py
@@ -3,8 +3,9 @@ import turtle
 
 class FetchName(turtle.Turtle):
     def __init__(self, answer, x, y):
-        super().__init__()
-        self.penup()
-        self.hideturtle()
-        self.goto(x, y)
+        super().__init__()  # Initialize the turtle object
+        self.penup()  # Lift the pen to avoid drawing while moving
+        self.hideturtle()  # Hide the turtle cursor
+        self.goto(x, y)  # Move the turtle to the specified coordinates (x, y)
+        # Write the provided state name on the screen at the current position
         self.write(f"{answer}", False, font=("Arial", 7, "bold"))
